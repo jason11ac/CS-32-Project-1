655227d53a617ad34840cf8e2251ca241df4ea69
jason11ac <jason11ac@gmail.com>
2015-08-26 13:52:03 -0700

    globals for the game

---
+++ globals.h
@@ -0,0 +1,17 @@
+#ifndef CS_32_Proj_1_globals_h
+#define CS_32_Proj_1_globals_h
+
+///////////////////////////////////////////////////////////////////////////
+// Manifest constants
+///////////////////////////////////////////////////////////////////////////
+
+const int MAXROWS = 20;             // max number of rows in the pit
+const int MAXCOLS = 40;             // max number of columns in the pit
+const int MAXSNAKES = 180;          // max number of snakes allowed
+
+const int UP    = 0;
+const int DOWN  = 1;
+const int LEFT  = 2;
+const int RIGHT = 3;
+
+#endif
