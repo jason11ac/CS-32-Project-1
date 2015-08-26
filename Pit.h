8ec04e7ab3fa875625a5d07595c2fb14cd81432f
jason11ac <jason11ac@gmail.com>
2015-08-26 13:46:44 -0700

    Header file for Pit

---
+++ Pit.h
@@ -0,0 +1,42 @@
+
+#ifndef CS_32_Proj_1_Pit_h
+#define CS_32_Proj_1_Pit_h
+
+#include "globals.h"
+#include <string>
+
+using namespace std;
+
+class Player;
+class Snake;
+
+class Pit
+{
+public:
+    // Constructor/destructor
+    Pit(int nRows, int nCols);
+    ~Pit();
+    
+    // Accessors
+    int     rows() const;
+    int     cols() const;
+    Player* player() const;
+    int     snakeCount() const;
+    int     numberOfSnakesAt(int r, int c) const;
+    void    display(string msg) const;
+    
+    // Mutators
+    bool   addSnake(int r, int c);
+    bool   addPlayer(int r, int c);
+    bool   destroyOneSnake(int r, int c);
+    bool   moveSnakes();
+    
+private:
+    int     m_rows;
+    int     m_cols;
+    Player* m_player;
+    Snake*  m_snakes[MAXSNAKES];
+    int     m_nSnakes;
+};
+
+#endif
